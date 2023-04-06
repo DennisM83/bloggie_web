cc0e7c3820e3154742329639e92c48f4f2cdc70f
Dennis Wenrich <dennisfinley@gmail.com>
2023-04-05 20:43:08 -0400

    setting up the DbContext from Entity

---
+++ Bloggie.Web/data/BloggieDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.data
+{
+	public class BloggieDbContext : DbContext
+	{
+		public BloggieDbContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+
+}
